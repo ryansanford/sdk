e41a4962311ff031a1cce9e8888b253992d32710
Harsha Kethineni <harshakethineni@flywheel.io>
2017-08-03 15:27:04 -0500

    added Search query and response List to whitelist

--- api/search.go
+++ api/search.go
@@ -8,9 +8,9 @@ import (
 type SearchQuery struct {
 	ReturnType string `json:"return_type,omitempty"` // REQUIRED file|acquisition|session|analysis
 
-	SearchString string   `json:"search_string,omitempty"` // OPTIONAL KEY any string including spaces and special characters
-	AllData      *bool    `json:"all_data,omitempty"`      // OPTIONAL, DEFAULTS TO FALSE true|false
-	Filters      []string `json:"filters,omitempty"`       // A LIST OF ES FILTERS, OPTIONAL KEY, find list of available filters here: https://www.elastic.co/guide/en/elasticsearch/reference/current/term-level-queries.html
+	SearchString string `json:"search_string,omitempty"` // OPTIONAL KEY any string including spaces and special characters
+	AllData      bool   `json:"all_data,omitempty"`      // OPTIONAL, DEFAULTS TO FALSE true|false
+	Filters      string `json:"filters,omitempty"`       // A LIST OF ES FILTERS, OPTIONAL KEY, find list of available filters here: https://www.elastic.co/guide/en/elasticsearch/reference/current/term-level-queries.html
 }
 
 func (c *Client) Search(search_query *SearchQuery) (*SearchResponseList, *http.Response, error) {
